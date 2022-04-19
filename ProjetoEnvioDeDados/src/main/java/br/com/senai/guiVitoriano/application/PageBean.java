453937544435d7dc10dc3673a629e6badf2077f4
Aluno <Aluno@DSN1251230721>
2022-04-19 17:17:27 -0300

    att

--- ProjetoEnvioDeDados/src/main/java/br/com/senai/guiVitoriano/application/PageBean.java
+++ ProjetoEnvioDeDados/src/main/java/br/com/senai/guiVitoriano/application/PageBean.java
@@ -1,1 +1,43 @@
 package br.com.senai.guiVitoriano.application;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.Flash;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+@SuppressWarnings("serial")
+
+public class PageBean implements Serializable{
+	
+	private String nome;
+	
+	@Inject
+	private Flash flash;
+	
+	public String processar() {
+		
+		flash.put("nomeDoUsuario", nome);
+		
+		return "result";
+		
+	}
+
+	public String getNome() {
+		
+		return nome;
+		
+	}
+
+	public void setNome(String nome) {
+		
+		this.nome = nome;
+		
+	}
+	
+	
+	
+}
