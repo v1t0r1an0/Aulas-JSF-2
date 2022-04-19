836c5dee378d8444d3bf3d7f9e02e354a98766d8
aluno <aluno@DSN1251230709>
2022-04-19 14:18:18 -0300

    atualizacao

---
+++ ProjetoNavegacaoTelas/src/main/java/br/com/senai/guiVitoriano/application/PageBean.java
@@ -0,0 +1,14 @@
+package br.com.senai.willianFerreira.application;
+
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+
+public class PageBean implements Serializable {
+
+}
